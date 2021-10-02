12a02a5d612698c0d9afa97e6ef5a8ed058848e0
unknown <you@example.comav>
2021-10-02 12:01:18 +0300

    fix commit.

--- avi-blog/src/App.js
+++ avi-blog/src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import BlogDetails from './components/BlogDetails';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import NotFound from './pages/NotFound';
+import Articles from './pages/Articles';
+import NewArticles from './pages/NewArticles';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <NewArticles />
+            </Route>
+            <Route path="/blog">
+              <Articles />
+            </Route>
+             <Route path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
